3818a851525d56151c563c544f9ec6c23f78f916
DSikes <david@sikespc.com>
2023-04-18 16:26:43 -0400

    Fixed tuning notes and reset PID parameters to re-tune

--- src/ThermostatSettings.h
+++ src/ThermostatSettings.h
@@ -21,14 +21,14 @@ public:
     const int FanTimeoutSeconds = 300;
 
     //Define the aggressive tuning parameters, which are used when we're not close to the setpoint
-    double AggressiveKp = 5;
-    double AggressiveKi = 0.2;
-    double AggressiveKd = 1;
+    double AggressiveKp = 0.0;
+    double AggressiveKi = 3.0;
+    double AggressiveKd = 0.2;
 
     //Define the conservative tuning parameters for when we get close to the setpoint
-    double ConservativeKp = 3.3555;      // Was 1
-    double ConservativeKi = 59.5;        // Was 0.05, 59.5 derived as minutes in one oscillation cycle. Seconds would be 3570
-    double ConservativeKd = 9.916;       // Ki / 6
+    double ConservativeKp = 0.0;      //
+    double ConservativeKi = 3.0;      //
+    double ConservativeKd = 0.2;        // Ki / 6
 
     double SwitchToConservativeMargin = 2.0;         // Value in process variable units (Temperature) to consider being
     // "Close to the setpoint"      Example: If setpoint is 50, and this is 5.0 PID will switch to conservative values
